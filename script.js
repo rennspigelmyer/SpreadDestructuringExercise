d6da1ede09099cef80afcfb7188c1251b8cf774d
rennspigelmyer <catherinespigelmyer95@gmail.com>
2021-10-18 16:10:15 -0400

    Completed Spread/Destructuring Exercise

--- script.js
+++ script.js
@@ -1,43 +1,102 @@
-// 1a 
-const mcuShows = ["Loki", "What if...?"];
+// 1a
+const mcuShows = ["Loki", "What If...?"];
 
 // 1b
 const starWarsShows = ["The Mandalorian", "The Bad Batch"];
 
-// 1c Using Spread
+// 1c 
 const disneyPlusShows = [
-    mcuShows,
-    starWarsShows,
+    ...mcuShows,
+    ...starWarsShows,
     "Monsters At Work"
 ];
 
 // 1d
 console.log(disneyPlusShows);
 
-
 // 2a 
-/* const netflixMovies = {};
-netflixMovies = ["Action"]= "Extraction"
-netflixMovies = ["Crime"]= "Irishman" */
-
-// Create a const variable called amazonPrimeMovies and set the value to an object with the following key value pairs (key: "action" value: "The Tomorrow War") & (key: "drama" value: "One Night In Miami")
 const netflixMovies = {
     action: "Extraction",
     crime: "The Irishman"
 }
 
 // 2b 
-// Create a const variable called amazonPrimeMovies and set the value to an object with the following key value pairs (key: "action" value: "The Tomorrow War") & (key: "drama" value: "One Night In Miami")
 const amazonPrimeMovies = {
     action: "The Tomorrow War",
     drama: "One Night In Miami"
 }
 
-// 2c - Not Completed yet.
+// 2c 
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: "Hamilton",
+    //action: "Extraction"
+}
 
-const streamingMovies = [
-    netflixMovies,
-    amazonPrimeMovies,
-    //musical: "Hamilton",
+// 2d
+console.log(streamingMovies);
 
+
+// 3a
+const disneyJunior = [
+    "Mickey Mouse Clubhouse",
+    "Spidey and His Amazing Friends"
+];
+
+// 3b
+// const {mickey, spidey} = {mickey: "Mickey Mouse Clubhouse", spidey: "Spidey and His Amazing Friends"}
+const [mickey, spidey] = disneyJunior;
+
+// 3c 
+console.log(mickey, spidey);
+
+// 4a
+const avengers = {
+    warMachine: "James Rhodes",
+    theHulk: "Bruce Banner"
+};
+
+// 4b
+const {warMachine, theHulk} = avengers;
+
+// 4c
+//console.log(avengers);
+console.log(warMachine, theHulk);
+
+// 4d
+const moreAvengers = {
+    blackWidow: "Natasha Romanoff",
+    hawkeye: "Clint Barton",
+    ironMan: "Tony Stark"
+}
+
+// 4e
+const {blackWidow: nat, ...others} = moreAvengers;
+
+// 4f
+console.log(nat, others);
+
+
+// Bonus 
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+// 5a
+const all = [
+    ...bonus["first"],
+    ...bonus ["second"],
+    ...bonus ["third"]
 ]
+
+
+// 5b
+const {first, second, third} = bonus;
+const [one, two, three] = first;
+const [four, five, six] = second;
+const [seven, eight, nine] = third;
+
+console.log(one, two, three, four, five, six, seven, eight, nine);
